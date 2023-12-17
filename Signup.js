9243af5317d4712b8374766a55c20dec14916f66
Emmanuel <ibekweemmanuel12@gmail.com>
2023-12-17 01:11:57 -0800

    second commit

---
+++ Signup.js
@@ -0,0 +1,43 @@
+const togglePassword =  
+        document.querySelector('#togglePassword'); 
+          
+    const password = document.querySelector('#password'); 
+  
+    togglePassword.addEventListener('click', function (e) { 
+  
+        // Toggle the type attribute 
+        const type = password.getAttribute( 
+            'type') === 'password' ? 'text' : 'password'; 
+        password.setAttribute('type', type); 
+  
+        // Toggle the eye slash icon 
+        if (togglePassword.src.match( 
+"https://img.icons8.com/?size=24&id=96181&format=png")) {  
+            togglePassword.src = 
+"https://img.icons8.com/?size=24&id=85028&format=png"; 
+        } else { 
+            togglePassword.src = 
+"https://img.icons8.com/?size=24&id=85028&format=png"; 
+        } 
+    }); 
+
+
+
+
+
+
+
+
+
+// var a;
+// function pass(){
+//     if(a==1){
+//         document.getElementById('password').type='password';
+//         document.getElementById('pass-icon').src='pass-off.png';
+//         a=0;
+//     }else{
+//         document.getElementById('password').type='password';
+//         document.getElementById('pass-icon').src='pass-on.png';
+//         a=1;
+//     }
+// }
